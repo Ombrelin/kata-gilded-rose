29488facd496be53f4a242a6b22f0d6d0ad8c0b4
Arsène Lapostolet <arsene@lapostolet.fr>
2022-01-11 15:19:00 +0100

    implemented itemfactory

---
+++ cs/GildedRose/ItemFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace GildedRoseKata;
+
+public class ItemFactory
+{
+    public static Item BuildItem(Item item) =>
+        item.Name switch
+        {
+            "Aged Brie" => new CheeseItem {Name = item.Name, Quality = item.Quality},
+            "Backstage passes to a TAFKAL80ETC concert" => new BackstageItem {Name = item.Name, Quality = item.Quality},
+            "Sulfuras, Hand of Ragnaros" => new LegendayItem {Name = item.Name, Quality = item.Quality}
+            _ => item
+        };
+}
---
+++ cs/GildedRoseTests/ItemFactoryTest.cs
@@ -0,0 +1,37 @@
+﻿using GildedRoseKata;
+using Xunit;
+
+namespace GildedRoseTests;
+
+public class ItemFactoryTest
+{
+    [Fact]
+    public void Build_CheeseName_BuildsCheeseItem()
+    {
+        // When
+        Item item = ItemFactory.BuildItem(new Item(){Name = "Aged Brie"});
+
+        // When
+        Assert.IsType<CheeseItem>(item);
+    }
+    
+    [Fact]
+    public void Build_BackstageName_BuildsBackstageItem()
+    {
+        // When
+        Item item = ItemFactory.BuildItem(new Item(){Name = "Backstage passes to a TAFKAL80ETC concert"});
+
+        // When
+        Assert.IsType<BackstageItem>(item);
+    }
+    
+    [Fact]
+    public void Build_LegendaryName_BuildsLegendaryItem()
+    {
+        // When
+        Item item = ItemFactory.BuildItem(new Item(){Name = "Sulfuras, Hand of Ragnaros"});
+
+        // When
+        Assert.IsType<LegendayItem>(item);
+    }
+}
